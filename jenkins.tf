3be97517a5301f1e085dd6134c1ef28a32912956
Madina89 <madinalinux89@gmail.com>
2019-08-07 19:26:35 -0500

    instance type added

--- jenkins.tf
+++ jenkins.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "jenkins"{
     ami = "${var.instance_type}"
+    instance_type = "${var.instance_type}"
     key_name = "${aws_key_pair.developer_key.id}"
     associate_public_ip_address = true
     security_groups = ["${aws_security_group.jenkins.id}"]
