a1778029f9294f5ad87e8aba535656857405c375
Madina89 <madinalinux89@gmail.com>
2019-08-07 19:55:26 -0500

    updated

--- bakend.tf
+++ bakend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3"{
-        region = "us-east-2"
-    bucket = "infra-april-madina"
+        region = "eu-west-1"
+    bucket = "jenkins-2019"
     key = "infra.state"   
     }
 }
